272b4580979a3a3d46157b807c1035f3c3076796
Bhaswanth67 <bhaswanth67@gmail.com>
2025-12-04 12:49:03 +0530

    Add all EMS projects

---
+++ EMS-Frontend-Capstone/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ems.portal',
+  appName: 'EMS-Portal',
+  webDir: 'build'
+};
+
+export default config;
